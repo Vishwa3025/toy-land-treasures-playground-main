9973e41f6348f7d00aaa4d6ae341a224061e36a0
Vishwa R <89925780+Vishwa3025@users.noreply.github.com>
2025-09-26 11:10:44 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
+require("dotenv").config()
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "✅ Set" : "❌ Not Set");
+console.log("PORT:", process.env.PORT);
+
 const express = require("express");
 const path = require('path');
 const cors = require("cors");
